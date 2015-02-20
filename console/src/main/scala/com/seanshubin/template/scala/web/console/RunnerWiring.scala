9256afc2913241c22eaead8a73885dbb3f58b4b1
SeanShubin <sshubin@cj.com>
2015-02-19 16:41:44 -0800

    Renamed gate to route

--- console/src/main/scala/com/seanshubin/template/scala/web/console/RunnerWiring.scala
+++ console/src/main/scala/com/seanshubin/template/scala/web/console/RunnerWiring.scala
@@ -12,11 +12,11 @@ trait RunnerWiring {
   lazy val classLoaderReceiver: Receiver = new ClassLoaderReceiver(
     classLoader, configuration.classLoaderPrefix, configuration.contentByExtension, configuration.overridePath)
   lazy val echoReceiver: Receiver = new EchoReceiver()
-  lazy val redirectGate:Gate = new RedirectGate("redirect", redirectReceiver, configuration.redirectFunction)
-  lazy val classLoaderGate:Gate = new ClassLoaderGate(
+  lazy val redirectRoute:Route = new RedirectRoute("redirect", redirectReceiver, configuration.redirectFunction)
+  lazy val classLoaderRoute:Route = new ClassLoaderRoute(
     "class-loader", classLoaderReceiver, configuration.contentByExtension)
-  lazy val gates:Seq[Gate] = Seq(redirectGate, classLoaderGate)
-  lazy val dispatcher:Receiver = new DispatchingReceiver(gates)
+  lazy val routes:Seq[Route] = Seq(redirectRoute, classLoaderRoute)
+  lazy val dispatcher:Receiver = new DispatchingReceiver(routes)
   lazy val clock:Clock = new ClockImpl()
   lazy val notifications:Notifications = new NotificationsImpl(clock)
   lazy val receiver: Receiver = new FallbackReceiver(
